3987eeaca17c2071a4b456ea258e698d6d597c5c
Tomas Stary <tomichec@gmail.com>
2016-02-05 10:59:43 +0000

    adds security break; moves initialisation into trackMotor fcn

--- stepper.py
+++ stepper.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 #--------------------------------------
 #
-# Author : Tomas Stary
-# Date   : Sat Sep  6 18:29:14 UTC 2014
-# License: GNU GPLv3
+# Author   : Tomas Stary
+# Date     : Sat Sep  6 18:29:14 UTC 2014
+# Modified : Fri  5 Feb 10:47:32 GMT 2016
+# License  : GNU GPLv3
 #
 #--------------------------------------
 
@@ -11,35 +12,21 @@
 from time import sleep
 import RPi.GPIO as GPIO
 
-##################################################
-# Define the functions
-def init():
-  '''Setup pins as output pins of the motor'''
-  # Use board GPIO references 
-  GPIO.setmode(GPIO.BOARD)
-  
-  # Set all pins as output
-  print "Setting up the output pins."
-  for pin in PINS:
-    GPIO.setup(pin,GPIO.OUT)
-    GPIO.output(pin, 0)
-
 def RotateInner(delay = 0.002):
   '''Rotates the inner motor once following the information specified in
   sequence array.  Wait 'delay' ms to allow the movement of the motor.
 
   '''
   for line in sequence:
-    for index in range(len(PINS)):
-      # this sets up the output pins according to the information on the line
-      GPIO.output(PINS[index], line[index])
+    for pin, signal in zip(PINS, line):
+      # this sets up the output pins according to the signal on the line
+      GPIO.output(pin, signal)
 
     # take a break to allow the rotation of the axis
     sleep(delay)
 
-def trackMotor(rotations, sequence):
+def trackMotor():
   ''' Move the motor according to the signals send from the driver.'''
-  global MOVE
   # forward rotation
   forward = [[1,0,0,0],
              [1,1,0,0],
@@ -49,21 +36,42 @@ def trackMotor(rotations, sequence):
              [0,0,1,1],
              [0,0,0,1],
              [1,0,0,1]]
-  
   # backward rotation
   backward = forward[:] # copy the list
   backward.reverse()    # reverse the list in place
 
-  while (MOVE != 'Q') or (MOVE != 'q'):
-    if MOVE == 'F':
-      RotateInner(forward)
-    if MOVE == 'B':
-      RotateInner(backward)
+  global MOVE_SIGNAL  # variable to be changed from parent function
+  # initialise security stop to avoid full rotations which could
+  # break the Rpi and camera
+  sec_stop = 256
+  sec_count = sec_count//2
+
+  # Use board GPIO references 
+  GPIO.setmode(GPIO.BOARD)
+  # Set all pins as output
+  for pin in PINS:
+    GPIO.setup(pin,GPIO.OUT)
+    GPIO.output(pin, 0)
+  print "Output pins set up, waiting for instruction."
+
+  # rotation loop
+  while (MOVE_SIGNAL != 'Q') or (MOVE_SIGNAL != 'q'):
+    # evaluate the signal and security counter
+    if (MOVE_SIGNAL == 'F') and (sec_count < sec_stop):
+      RotateInner(forward) # move forward
+      sec_count += 1
+    else if MOVE_SIGNAL == 'B'and (sec_count > 0):
+      RotateInner(backward) # move backward
+      sec_count -= 1
+    else:
+      sleep(0.02) # free parking
     
   # switch off the motor
   for pin in PINS:
     GPIO.output(pin, 0)
   GPIO.cleanup()
+  
+  print "GPIO cleaned up. The function will terminate."
   
 ##################################################
 # Define properties of the motor
